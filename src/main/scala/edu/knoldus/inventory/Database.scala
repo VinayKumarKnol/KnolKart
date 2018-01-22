5208b0c4cdec3b086ea0478e1347bdfb3478c009
Vinay Kumar <vinay.kumar@knoldus.in>
2018-01-22 13:16:48 +0530

    Added Database file

--- src/main/scala/edu/knoldus/inventory/Database.scala
+++ src/main/scala/edu/knoldus/inventory/Database.scala
@@ -1,5 +1,42 @@
 package edu.knoldus.inventory
 
-class Database {
+import java.io.{File, PrintWriter}
+import org.apache.log4j.Logger
+import org.json4s.DefaultFormats
+import org.json4s.jackson.Serialization._
+
+import scala.io.Source
+
+object Database {
+  implicit def formats: DefaultFormats = DefaultFormats
+
+  val log: Logger = Logger.getLogger(this.getClass)
+
+  def writeToJSON(inventory: List[Items]): Boolean = {
+
+    try {
+      val writer = new PrintWriter(new File("database.json"))
+      val json = write(inventory)
+      writer.write(json)
+      writer.close()
+      true
+    } catch {
+      case except: Exception => log.info(s"\nError: ${except.getMessage}")
+        false
+    } finally {
+
+    }
+  }
+
+  def readFromJSON(): List[Items] = {
+    try {
+      val bufferedSource = Source.fromFile(new File("Secret.json")).mkString
+      val updatedList: List[Items] = read[List[Items]](bufferedSource)
+      updatedList
+    } catch {
+      case except: Exception => log.info(s"\nError: ${except.getCause}")
+        List[Items]()
+    }
+  }
 
 }
--- src/main/scala/edu/knoldus/inventory/Items.scala
+++ src/main/scala/edu/knoldus/inventory/Items.scala
@@ -2,8 +2,13 @@ package edu.knoldus.inventory
 
 class Items(
              val id: Int,
+             val vendorId: Int,
              val name: String,
              val price: Double,
              val quantity: Int,
              val category: String
-           )
+           ){
+
+
+
+}
