2e0584d5c2f9d8c9537ad6eb0abd3936c9cab0d0
Sudeep James Tirkey <sudeep.james@knoldus.in>
2018-01-22 14:47:37 +0530

    Customer

---
+++ src/main/scala/edu/knoldus/inventory/Customer.scala
@@ -0,0 +1,8 @@
+package edu.knoldus.inventory
+
+class Customer (
+               val id: Int,
+               val name: String,
+               val address: String) {
+  def shoppingList (): List[Items]
+}
