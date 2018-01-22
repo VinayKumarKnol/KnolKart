3b917a14b36abf138745c881a609a62ec498c318
Vinay Kumar <vinay.kumar@knoldus.in>
2018-01-22 16:08:34 +0530

    "Made a minor change to sortByID"

--- src/main/scala/edu/knoldus/inventory/sortImplicitMethods.scala
+++ src/main/scala/edu/knoldus/inventory/sortImplicitMethods.scala
@@ -11,9 +11,9 @@ object sortImplicitMethods {
   }
 
   implicit def sortById(list: List[Person])(direction: String): List[Person] = {
-
-    case "high to low" => list.sortWith((first, second) => first.id > second.id)
-    case "low to high" => list.sortWith((first, second) => first.id < second.id)
-
+    direction match {
+      case "high to low" => list.sortWith((first, second) => first.id > second.id)
+      case "low to high" => list.sortWith((first, second) => first.id < second.id)
+    }
   }
 }
