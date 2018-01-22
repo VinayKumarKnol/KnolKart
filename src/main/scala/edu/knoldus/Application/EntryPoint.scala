2bbcd4e419e8f3b939e3f4d463bafb32870b0856
Sudeep James Tirkey <sudeep.james@knoldus.in>
2018-01-22 15:19:35 +0530

    First Draft of EntryPoint

--- src/main/scala/edu/knoldus/Application/EntryPoint.scala
+++ src/main/scala/edu/knoldus/Application/EntryPoint.scala
@@ -1,5 +1,35 @@
 package edu.knoldus.Application
 
-object EntryPoint {
+import org.apache.log4j.Logger
 
+import scala.io.StdIn
+
+class EntryPoint{
+  val log: Logger = Logger.getLogger(this.getClass)
+
+  def customerModule() = {
+    log.info(s"\nEnter Choice :\n1. Search Item\n2. ")
+    log.info(s"\nYour Choice : ")
+    val choice = StdIn.readInt()
+    choice match {}
+  }
+  def inventoryModule() = {
+    log.info(s"\nEnter Choice :\n1. Update Items \n2. Delete Items")
+    log.info(s"\nYour Choice : ")
+    val choice = StdIn.readInt()
+    choice match {}
+  }
+}
+
+object EntryPoint {
+  val entry = new EntryPoint
+  val log: Logger = Logger.getLogger(this.getClass)
+  log.info(s"\nEnter Choice :\n1. Customer Module\n2. Inventory Module")
+  log.info(s"\nYour Choice : ")
+  val choice = StdIn.readInt()
+  choice match {
+    case 1 => entry.customerModule()
+    case 2 => entry.inventoryModule()
+    case _ => log.info(s"Wrong Choice")
+  }
 }
