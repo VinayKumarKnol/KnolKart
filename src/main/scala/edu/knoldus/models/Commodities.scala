9fd9ba6695edc6aa5419a1f48301b832011c87c7
Sudeep James Tirkey <sudeep.james@knoldus.in>
2018-01-29 10:09:15 +0530

    Partial Complete

--- src/main/scala/edu/knoldus/models/Commodities.scala
+++ src/main/scala/edu/knoldus/models/Commodities.scala
@@ -3,5 +3,5 @@ package edu.knoldus.models
 abstract class Commodities {
   val id: Int
 
-  def updateAttribute(attribute: String, newValue: String)
+  def updateAttribute(attribute: String, newValue: String): Commodities
 }
